c697679b36d12e44f7fb3f19660b652974fbc86d
RemyMach <remygt@hotmail.fr>
2021-05-06 14:57:49 +0200

    add admin middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -19,3 +19,23 @@ export async function authMiddleware(req: express.Request, res: express.Response
         res.status(401).end();
     }
 }
+
+export async function adminAuthMiddleware(req: express.Request, res: express.Response, next: express.NextFunction) {
+    const auth = req.headers["authorization"];
+
+    if(auth !== undefined) {
+        const token = auth.replace('Bearer ', '');
+        const authController = await AuthController.getInstance();
+        const session = await authController.getSpecificRoleSession(token, ['admin']);
+
+        if(session !== null) {
+            next();
+            return;
+        } else {
+            res.status(403).end();
+            return;
+        }
+    } else {
+        res.status(401).end();
+    }
+}
